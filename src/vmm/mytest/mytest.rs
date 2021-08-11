8cc431c7a3c7b9b78683c92f436d2c50ba8ecc2b
Amory Hoste <amory.hoste@gmail.com>
2021-08-11 13:48:14 +0200

    remove unused imports

--- src/vmm/mytest/mytest.rs
+++ src/vmm/mytest/mytest.rs
@@ -1,17 +1,8 @@
 use std::fs::File;
 use std::path::Path;
 use snapshot::Snapshot;
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use std::thread;
-use std::time::Duration;
-use utils::tempfile::TempFile;
-use versionize::VersionMap;
 use vmm::persist::MicrovmState;
-use vmm::utilities::mock_resources::NOISY_KERNEL_IMAGE;
-use vmm::utilities::test_utils::create_vmm;
 use vmm::version_map::VERSION_MAP;
-use vmm::vmm_config::snapshot::{CreateSnapshotParams, SnapshotType};
-use vmm::{persist, FC_EXIT_CODE_OK};
 
 fn main() {
     let snapshot_path = Path::new("snapfile");
