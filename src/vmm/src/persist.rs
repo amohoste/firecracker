230c29b465414d6030e3c8e892fdea037a8e6495
Amory Hoste <amory.hoste@gmail.com>
2021-08-29 13:20:40 +0200

    more prints

--- src/vmm/src/persist.rs
+++ src/vmm/src/persist.rs
@@ -393,6 +393,7 @@ pub fn restore_from_snapshot(
 
     let new_snapshot_path = &params.new_snapshot_path;
 
+    println!("New snapshot path {0}", new_snapshot_path);
     if ! new_snapshot_path.eq("") {
         let n = microvm_state.device_states.block_devices.len();
         for i in 1..n {
